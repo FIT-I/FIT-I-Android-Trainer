38779c92a6e5b7675bdd23475c19a17f372b5b73
yoon-gang00 <sgi07151@naver.com>
2023-01-19 15:12:49 +0900

    feat : 수정하기 눌렀을때 나오는 페이지, edittext로 변경, 완료버튼/뒤로가기 버튼 누르면 프로필로 돌아가게하기 #4

--- app/src/main/java/com/example/fit_i_trainer/ProfileModifyMeActivity.kt
+++ app/src/main/java/com/example/fit_i_trainer/ProfileModifyMeActivity.kt
@@ -1,4 +1,28 @@
 package com.example.fit_i_trainer
 
-class ProfileModifyMeActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileModifyMeActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_modify_me)
+        //완료버튼
+        val done = findViewById<Button>(R.id.button_done)
+        done.setOnClickListener{
+            val intent = Intent(this,ProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        //뒤로가기
+        val goBack = findViewById<ImageButton>(R.id.back_arrow)
+        goBack.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
