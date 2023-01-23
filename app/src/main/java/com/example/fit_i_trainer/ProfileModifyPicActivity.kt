e2c3432303d42f5303bd1bea023db206d477b618
yoon-gang00 <sgi07151@naver.com>
2023-01-23 16:58:25 +0900

    fix : 안되는기능들 고치기 #4

--- app/src/main/java/com/example/fit_i_trainer/ProfileModifyPicActivity.kt
+++ app/src/main/java/com/example/fit_i_trainer/ProfileModifyPicActivity.kt
@@ -1,4 +1,38 @@
 package com.example.fit_i_trainer
 
-class ProfileModifyPicActivity {
+import android.Manifest
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import coil.load
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileModifyPicActivity: AppCompatActivity() {
+    private val permissionList = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val checkPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+        result.forEach {
+            if(!it.value) {
+                Toast.makeText(applicationContext, "권한 동의 필요!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+    val btnAlbum = findViewById<ImageButton>(R.id.btn_album)
+    val imgLoad = findViewById<ImageView>(R.id.img_load)
+    private val readImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        imgLoad.load(uri)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkPermission.launch(permissionList)
+
+        btnAlbum.setOnClickListener {
+            readImage.launch("image/*")
+        }
+    }
 }
