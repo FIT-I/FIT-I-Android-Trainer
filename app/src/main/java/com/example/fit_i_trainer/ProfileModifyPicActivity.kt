aa5323624af917d24a0dd885b3de590fc411eefd
yoon-gang00 <sgi07151@naver.com>
2023-01-27 15:14:22 +0900

    refactoring :  #4

--- app/src/main/java/com/example/fit_i_trainer/ProfileModifyPicActivity.kt
+++ app/src/main/java/com/example/fit_i_trainer/ProfileModifyPicActivity.kt
@@ -1,38 +1,44 @@
 package com.example.fit_i_trainer
 
 import android.Manifest
+import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.Toast
-import coil.load
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fit_i_trainer.databinding.ActivityProfileModifyPicBinding
 
 class ProfileModifyPicActivity: AppCompatActivity() {
-    private val permissionList = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-    private val checkPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
-        result.forEach {
-            if(!it.value) {
-                Toast.makeText(applicationContext, "권한 동의 필요!", Toast.LENGTH_SHORT).show()
+    var photoUri: Uri? = null
+    lateinit var storagePermission:ActivityResultLauncher<String>
+    lateinit var galleryLauncher:ActivityResultLauncher<String>
+    val binding by lazy{ ActivityProfileModifyPicBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_modify_pic)
+        storagePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            isGranted ->
+            if(isGranted){
+                setViews()
+            }else{
+                Toast.makeText(baseContext,"외부 저장소 권한을 승인해야 앱을 사용할 수 있습니다.", Toast.LENGTH_LONG).show()
                 finish()
             }
         }
-    }
-    val btnAlbum = findViewById<ImageButton>(R.id.btn_album)
-    val imgLoad = findViewById<ImageView>(R.id.img_load)
-    private val readImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-        imgLoad.load(uri)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        checkPermission.launch(permissionList)
-
-        btnAlbum.setOnClickListener {
-            readImage.launch("image/*")
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+//                uri->
+//            binding.imagePreview.setImageURI(uri)
         }
+        storagePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+    fun setViews(){
+        binding.btnAlbum.setOnClickListener{
+            openGallery()
+        }
+    }
+    fun openGallery(){
+        galleryLauncher.launch("image/*")
+
     }
 }
