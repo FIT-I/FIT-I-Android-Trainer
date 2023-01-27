297a154bde80a8ecfa340aedd97a79e93f67df25
Yujin <jini1514@soongsil.ac.kr>
2023-01-27 11:28:19 +0900

    fix : 비밀번호 가시화/비가시화 구현 방법 변경

---
+++ app/src/main/java/com/example/fit_i_trainer/MypageChangePwActivity.kt
@@ -0,0 +1,159 @@
+package com.example.fit_i_trainer
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.widget.*
+import com.example.fit_i_trainer.databinding.ActivityMypageChangePwBinding
+import java.util.regex.Pattern
+
+class MypageChangePwActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMypageChangePwBinding
+
+    var pw1: String=""
+    var pw2: String=""
+
+    val pwPattern = "^.*(?=^.{5,15}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*$"
+
+    private lateinit var confirmPW: TextView //비밀번호 일치
+    private lateinit var special: TextView //비밀번호 정규성
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage_change_pw)
+
+        binding = ActivityMypageChangePwBinding.inflate(layoutInflater)
+
+        val etPw1 : EditText = findViewById(R.id.et_newPw1)
+        val etPw2 : EditText = findViewById(R.id.et_newPw2)
+
+        confirmPW = findViewById(R.id.tv_confirmC)
+        special = findViewById(R.id.tv_specialC)
+
+        val btnFinPwChange : Button = findViewById(R.id.btn_fin_pw_change)
+
+        btnFinPwChange.isEnabled = false
+
+        etPw1.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            //값 변경 시 실행되는 함수
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //입력값 담기
+                pw1 = etPw1.text.toString()
+
+                //stroke 색상변경
+                if (pw1.isNotEmpty())
+                    etPw1.setBackgroundResource(R.drawable.custom_edittext_border)
+                else
+                    etPw1.setBackgroundResource(R.drawable.custom_edittext_border)
+
+                //값 유무에 따른 활성화 여부
+                btnFinPwChange.isEnabled = isTrue()
+            }
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+
+        etPw2.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            //값 변경 시 실행되는 함수
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //입력값 담기
+                pw2 = etPw2.text.toString()
+
+                //stroke 색상변경
+                if (pw2.isNotEmpty())
+                    etPw2.setBackgroundResource(R.drawable.custom_edittext_border)
+                else
+                    etPw2.setBackgroundResource(R.drawable.custom_edittext_border)
+
+                btnFinPwChange.isEnabled = isTrue() //있다면 true 없으면 false
+
+            }
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+
+        //비밀번호 눈 아이콘
+//
+//        val ivEye1: ImageView = findViewById(R.id.ib_eye1)
+//        val ivEye2: ImageView = findViewById(R.id.ib_eye2)
+//
+//        ivEye1.setOnClickListener() {
+//            if (ivEye1.tag.equals("0")) {//비밀번호 안 보이고 있던 상황
+//                ivEye1.tag = "1"
+//                ivEye1.setImageResource(R.drawable.ic_eye)
+//                etPw1.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+//            }
+//            if(ivEye1.tag.equals("1")) {//비밀번호 보이고 있던 상황
+//                ivEye1.tag = "0"
+//                ivEye1.setImageResource(R.drawable.ic_eye_slash)
+//                etPw1.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+//            }            //etPW.setSelection(etPW.text.length)
+//            etPw1.setSelection(etPw1.text.length)
+//        }
+//
+//        ivEye2.setOnClickListener() {
+//            if (ivEye2.tag.equals("0")) {//비밀번호 안 보이고 있던 상황
+//                ivEye2.tag = "1"
+//                ivEye2.setImageResource(R.drawable.ic_eye)
+//                etPw2.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+//            }
+//            if(ivEye2.tag.equals("1")) {//비밀번호 보이고 있던 상황
+//                ivEye2.tag = "0"
+//                ivEye2.setImageResource(R.drawable.ic_eye_slash)
+//                etPw2.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+//            }
+//            //etPW.setSelection(etPW.text.length)
+//            etPw2.setSelection(etPw2.text.length)
+//        }
+
+
+        //비밀번호 변경 끝!
+        //버튼 이벤트
+        btnFinPwChange.setOnClickListener {  val intent = Intent(this, LoginSplashActivity::class.java)
+            startActivity(intent)  // 화면 전환을 시켜줌
+            finish()
+            Toast.makeText(this, pw1 + "findPW", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isTrue(): Boolean {
+        pwDoubleCheck()
+        pwCheck()
+        return pw1.isNotEmpty() && pw2.isNotEmpty()
+    }
+
+    //비밀번호 정규성 검사
+    private fun pwCheck(): Boolean {
+        val pattern2 = Pattern.compile(pwPattern) // 패턴 컴파일
+        val matcher2 = pattern2.matcher(pw1)
+
+        return if (!matcher2.find()) {
+            special.text = "(영문, 숫자, 특수문자(! @ # \$ % ^ & + =) 를 포함해 5자 이상으로 입력해주세요)"
+            special.setTextColor(Color.parseColor("#FF0000"))
+            false
+        } else {
+            special.text = " "
+            //special.setTextColor(Color.parseColor("#D9D9D9"))
+            true
+        }
+    }
+
+    //패스워드 일치 검사 로직
+    private fun pwDoubleCheck(): Boolean {
+        if (pw1 == pw2) {
+            confirmPW.text = " "
+            return true
+        } else {
+            confirmPW.text = "비밀번호가 일치하지 않습니다."
+            confirmPW.setTextColor(Color.parseColor("#FF0000"))
+            return false
+        }
+    }
+}
+
