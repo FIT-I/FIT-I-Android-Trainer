59376f32787fa96ce9dcf09026a792b2a956f029
Yujin <jini1514@soongsil.ac.kr>
2023-02-02 01:26:17 +0900

    feat : Home 바인딩 중 #14

--- app/src/main/java/com/example/fit_i_trainer/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/fit_i_trainer/ui/main/home/HomeFragment.kt
@@ -1,60 +1,71 @@
 package com.example.fit_i_trainer.ui.main.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.fit_i_trainer.R
+import com.example.fit_i_trainer.RetrofitImpl
+import com.example.fit_i_trainer.data.model.response.GetTrainerInfoResponse
+import com.example.fit_i_trainer.data.service.TrainerService
+import com.example.fit_i_trainer.databinding.FragmentHomeBinding
+import com.example.fit_i_trainer.ui.profile.ProfileActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = requireNotNull(_binding) { "FragmentHomeBinding" }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        fun onBind(data: GetTrainerInfoResponse.Result) {
+            binding.tvName.text=data.name
+            //binding.tvEmail.text=data.
+            binding.tv
+        }
+
+        val trainerService = RetrofitImpl.getApiClient().create(TrainerService::class.java)
+        trainerService.getTrainerInfo().enqueue(object :
+            Callback<GetTrainerInfoResponse> {
+            override fun onResponse(
+                call: Call<GetTrainerInfoResponse>,
+                response: Response<GetTrainerInfoResponse>
+            ) {
+                if (response.isSuccessful) {
+                    // 정상적으로 통신이 성공된 경우
+                    onBind(response.body()!!.result)
+                    Log.d("post", "onResponse 성공: " + response.body().toString());
+                    //Toast.makeText(this@ProfileActivity, "비밀번호 찾기 성공!", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    Log.d("post", "onResponse 실패")
                 }
             }
+
+            override fun onFailure(call: Call<GetTrainerInfoResponse>, t: Throwable) {
+                // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
+                Log.d("post", "onFailure 에러: " + t.message.toString());
+            }
+        })
+
     }
 }
