30bad7dad7d8e9d69bad19a3bd63dd88ca44d712
gurino <2021111786@dgu.ac.kr>
2023-02-03 15:51:31 +0900

    feat : mypage 갤러리 연동 추가

--- app/src/main/java/com/example/fit_i_trainer/MypageModifyProfileFragment.kt
+++ app/src/main/java/com/example/fit_i_trainer/MypageModifyProfileFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.fragment.app.FragmentTransaction
+import com.example.fit_i.ProfileDialog
 
 
 class MypageModifyProfileFragment : Fragment() {
@@ -16,29 +17,31 @@ class MypageModifyProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_mypage_modify_profile,container,false)
+        val view = inflater.inflate(R.layout.fragment_mypage_modify_profile, container, false)
         val ibpre = view.findViewById<View>(R.id.ib_pre)
         val btnphoto = view.findViewById<View>(R.id.btn_click_photo)
 
         //이전
-        ibpre.setOnClickListener{
+        ibpre.setOnClickListener {
             val mypageFragment = MypageFragment()
-            val transaction : FragmentTransaction = requireFragmentManager().beginTransaction()
+            val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
 
             //이전 화면으로 이동
-            transaction.replace(R.id.fl_container,mypageFragment)
+            transaction.replace(R.id.fl_container, mypageFragment)
             transaction.commit()
 
         }
-        btnphoto.setOnClickListener{
-            val photoDialog = PhotoDialog()
-            val transaction : FragmentTransaction = requireFragmentManager().beginTransaction()
-            transaction.commit()
+        btnphoto.setOnClickListener {
+            showProfileDialog()
         }
         return view
     }
 
+    private fun showProfileDialog() {
+        ProfileDialog(requireContext()) {
+
+        }.show()
 
 
-
+    }
 }
