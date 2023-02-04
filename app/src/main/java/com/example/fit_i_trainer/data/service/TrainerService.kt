7633f369fb652d871e6d389f8185d86c43a373b8
yoon-gang00 <sgi07151@naver.com>
2023-02-05 02:55:12 +0900

    Feat : 트레이너 정보수정 소개글 API 401오류,, #4

--- app/src/main/java/com/example/fit_i_trainer/data/service/TrainerService.kt
+++ app/src/main/java/com/example/fit_i_trainer/data/service/TrainerService.kt
@@ -4,20 +4,19 @@ import com.example.fit_i_trainer.data.model.request.ModifyTrainerCategoryRequest
 import com.example.fit_i_trainer.data.model.request.ModifyTrainerInfoRequest
 import com.example.fit_i_trainer.data.model.response.BaseResponse
 import com.example.fit_i_trainer.data.model.response.GetTrainerInfoResponse
+import com.example.fit_i_trainer.data.model.response.ModifyTrainerInfoResponse
 import retrofit2.Call
 import retrofit2.http.*
 
 interface TrainerService {
 
     //트레이너 개인정보조회
-    @Headers("content-type: application/json")
     @GET("api/trainer/information")
     fun getTrainerInfo(): retrofit2.Call<GetTrainerInfoResponse>
 
     //트레이너 정보수정
-    @Headers("content-type: application/json")
     @PUT("api/trainer/information")
-    fun modifyTrainerInfo(@Body req : ModifyTrainerInfoRequest) : retrofit2.Call<BaseResponse>
+    fun modifyTrainerInfo(@Body req : ModifyTrainerInfoRequest) : retrofit2.Call<ModifyTrainerInfoResponse>
 
     //트레이너 사진 및 자격증 추가
     //@Headers("content-type: application/json")
@@ -25,32 +24,26 @@ interface TrainerService {
     //fun addTrainerEtcImg(@Body ectImage : Array<String>) : Call<BaseResponse>
 
     //트레이너 프로필 삭제
-    @Headers("content-type: application/json")
     @DELETE("api/trainer/profile")
     fun deleteTrainerProflie() : retrofit2.Call<BaseResponse>
 
     //트레이너 프로필수정
-    @Headers("content-type: application/json")
     @PATCH("api/trainer/profile")
     fun modifyTrainerProfile(@Body profileImage : String) : retrofit2.Call<BaseResponse>
 
     //내 매칭 관리 on/off
-    @Headers("content-type: application/json")
     @PATCH ("api/trainer/mymatching")
     fun controlMatchingOnOff() : retrofit2.Call<BaseResponse>
 
     //트레이너 카테고리 수정
-    @Headers("content-type: application/json")
     @PATCH("api/trainer/category")
     fun modifyTrainerCategory(@Body req : ModifyTrainerCategoryRequest) : retrofit2.Call<BaseResponse>
 
     //트레이너 배경화면수정
-    @Headers("content-type: application/json")
     @PATCH ("api/trainer/bgimg")
     fun modifyTrainerBgImg(@Body backgroundImage : String) : retrofit2.Call<BaseResponse>
 
     //트레이너 사진 및 자격증 삭제
-    @Headers("content-type: application/json")
     @DELETE("api/trainer/etcimg/{etcImgIdx}")
     fun deleteTrainerEtcImg(@Path("etcImgIdx") etcImgIdx : Int) : retrofit2.Call<BaseResponse>
 
--- app/src/main/java/com/example/fit_i_trainer/ui/profile/ProfileModifyMeActivity.kt
+++ app/src/main/java/com/example/fit_i_trainer/ui/profile/ProfileModifyMeActivity.kt
@@ -4,17 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fit_i_trainer.R
+import com.example.fit_i_trainer.RetrofitImpl
+import com.example.fit_i_trainer.data.model.request.ModifyTrainerInfoRequest
+import com.example.fit_i_trainer.data.model.response.ModifyTrainerInfoResponse
+import com.example.fit_i_trainer.data.service.TrainerService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileModifyMeActivity :AppCompatActivity(){
+    var intro : String =""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_modify_me)
+        val name: String = intent.getStringExtra("name").toString()
+        val serviceDetail: String = intent.getStringExtra("serviceDetail").toString()
+        val cost : Int = intent.getIntExtra("cost",0).toInt()
         //객체생성
         val edit : EditText = findViewById(R.id.tv_about_me_context)
         val done = findViewById<Button>(R.id.btn_done)
@@ -38,18 +50,49 @@ class ProfileModifyMeActivity :AppCompatActivity(){
             }
 
             override fun afterTextChanged(p0: Editable?) {
+                intro = edit.text.toString()
                 done.isEnabled = true
+
             }
         })
 
         //완료버튼
         done.setOnClickListener{
+           //401번오류가 난다,,
+            // 토큰 인증이 필요하지만 안 한 경우 / (인증 잘 함 or 인증 필요X 상황에서) request 형식이 옳지 않은데 서버 내 validation 코드에서 잡아내지 못한 경우 => 이건 서버 쪽에 연락주시면 validation 코드 추가하겠습니다..!
+            val trainerService = RetrofitImpl.getApiClient().create(TrainerService::class.java)
+            trainerService.modifyTrainerInfo(
+                ModifyTrainerInfoRequest(cost,intro
+                ,name,serviceDetail)
+            ).enqueue(object :
+                Callback<ModifyTrainerInfoResponse> {
+                override fun onResponse(
+                    call: Call<ModifyTrainerInfoResponse>,
+                    response: Response<ModifyTrainerInfoResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        // 정상적으로 통신이 성공된 경우
+                        Log.d("put", "onResponse 성공: " + response.body().toString());
+                        //Toast.makeText(this@ProfileActivity, "비밀번호 찾기 성공!", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                        Log.d("put", "onResponse 실패"+response.code())
+                    }
+                }
+
+                override fun onFailure(call: Call<ModifyTrainerInfoResponse>, t: Throwable) {
+                    // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
+                    Log.d("put", "onFailure 에러: " + t.message.toString());
+                }
+            })
 //            val intent1 = Intent(this,ProfileAboutMeActivity::class.java)
 //            intent1.putExtra("contextMe",edit.text.toString())
 //            startActivity(intent1)
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
             finish()
+
         }
         //뒤로가기
         val goBack = findViewById<ImageButton>(R.id.ib_back_arrow)
