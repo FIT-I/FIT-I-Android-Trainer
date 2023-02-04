65eace6c1e8a35ec050a4e335fde26724e653fb8
gurino <2021111786@dgu.ac.kr>
2023-02-04 16:17:07 +0900

    feat : 매칭 리스트 api #17

--- app/src/main/java/com/example/fit_i_trainer/ui/matching/MatchingAdapter.kt
+++ app/src/main/java/com/example/fit_i_trainer/ui/matching/MatchingAdapter.kt
@@ -1,13 +1,15 @@
 package com.example.fit_i_trainer.ui.matching
 
-import android.util.Log
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.fit_i_trainer.data.model.response.GettrainerResponse
 import com.example.fit_i_trainer.databinding.ItemmatchingBinding
 
-class MatchingAdapter(private val dataList: ArrayList<MatchingData>):
+class MatchingAdapter(private val dataList: List<GettrainerResponse.Result>):
 RecyclerView.Adapter<MatchingAdapter.ViewHolder>() {
 
 
@@ -39,29 +41,34 @@ RecyclerView.Adapter<MatchingAdapter.ViewHolder>() {
 
 
         fun onBind(position: Int) {
-            binding.matchingIm.text = dataList[position].im
-            binding.matchingDate.text = dataList[position].date
-            binding.matchingMent.text = dataList[position].ment
+            binding.matchingIm.text = dataList[position].name
+            binding.matchingDate.text = dataList[position].orderDate
+            binding.matchingMent.text = dataList[position].pickUpType
 
-            itemView.setOnClickListener {
-                Log.d("Click", "success")
 
             }
 
         }
 
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(position)
+
+//        holder.itemView.setOnClickListener{
+//            val matchlistIntent = Intent(holder.itemView.context, MatchingFragment::class.java)
+//            startActivity(holder.itemView.context,matchlistIntent,null)
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val viewBinding =
-            ItemmatchingBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+       val viewBinding =
+           ItemmatchingBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return ViewHolder(viewBinding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.onBind(position)
-    }
-
-    override fun getItemCount(): Int = dataList.size
+    override fun getItemCount(): Int =
+        dataList.size
 
 }
+
+
+
+
