212c7e220dab256e9be431edec354298179b8e45
Yujin <jini1514@soongsil.ac.kr>
2023-02-06 00:09:52 +0900

    fix : 공지글 바인딩

--- app/src/main/java/com/example/fit_i_trainer/ui/main/mypage/notice/MypageNoticeMoreActivity.kt
+++ app/src/main/java/com/example/fit_i_trainer/ui/main/mypage/notice/MypageNoticeMoreActivity.kt
@@ -2,11 +2,22 @@ package com.example.fit_i_trainer.ui.main.mypage.notice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.fit_i_trainer.R
+import com.example.fit_i_trainer.databinding.ActivityMypageNoticeMoreBinding
 
 class MypageNoticeMoreActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMypageNoticeMoreBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mypage_notice_more)
+        binding = ActivityMypageNoticeMoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val data = intent.getParcelableExtra<NoticeData>("notice")
+
+        binding.tvNoticeTitle.text = data!!.title
+        binding.tvNoticeContent.text = data.contents
+        Log.d("post", data.toString())
     }
 }
